7e082b44ee2efff3c15d8ed98270a86623e827e0
Thomas-Dulompont <thomasdulompont.1@gmail.com>
2022-09-27 12:28:38 +0200

    Ajout fonction test

--- morpion.py
+++ morpion.py
@@ -13,10 +13,13 @@ def jouer_coup(grille:dict, joueur:str, coup:str):
     :param grille: (dict) Un dictionnaire qui représente une grille de morpion
     :param joueur: (str) une chaine de caractères qui contient O ou X
     :param coup: (str) une chaine de caractères qui contient une lettre entre A et C en première position, et un chiffre entre 0 et 2 en seconde position
-    :return: (Boolean) True si le coup est valide, False sinon
+    :return: (Boolean) True si le coup est valide, false sinon
     """
     if coup[0] in ["A", "B", "C"] and 0 <= int(coup[1]) <= 2:
         if grille[coup[0]][int(coup[1])] == "-":
             grille[coup[0]][int(coup[1])] = joueur
             return True
     return False
+
+def fonction_test():
+    pass
