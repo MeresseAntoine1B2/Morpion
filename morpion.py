90e122013ec76d87c92f2a849cb0fced8aeef508
MeresseAntoine1B2 <antoine@meresse.info>
2022-09-27 11:58:25 +0200

    Ajout de la fonction est_gagnante

--- morpion.py
+++ morpion.py
@@ -20,3 +20,27 @@ def jouer_coup(grille:dict, joueur:str, coup:str):
             grille[coup[0]][int(coup[1])] = joueur
             return True
     return False
+
+def est_gagnante(grille):
+    """
+    Fonction qui renvoie si la grille est gagnante ou non
+    :param grille: (dict) Un dictionnaire qui représente une grille de morpion
+    :return: (Boolean) True si la grille est gagnante, False sinon
+    """
+    # Vérification des lignes
+    for cle in grille:
+        if grille[cle][0] == grille[cle][1] == grille[cle][2] and grille[cle][0] != "-":
+            return True
+    
+    # Vérification des colonnes
+    for i in range(3):
+        if grille["A"][i] == grille["B"][i] == grille["C"][i] and grille["A"][i] != "-":
+            return True
+    
+    # Vérification des diagonales
+    if grille["A"][0] == grille["B"][1] == grille["C"][2] and grille["A"][0] != "-":
+            return True
+    if grille["A"][2] == grille["B"][1] == grille["C"][0] and grille["A"][2] != "-":
+            return True
+    
+    return False
